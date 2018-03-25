56fc8b83bfb5fa8547de6aa5a553d2f8f8943040
Joe <hoangjd@vcu.edu>
2018-03-25 10:58:22 -0400

    Balloon Animations

--- Kids_Joy_Center/Balloon/BalloonViewController.swift
+++ Kids_Joy_Center/Balloon/BalloonViewController.swift
@@ -18,11 +18,42 @@ class BalloonViewController: UIViewController {
     
     var ourDifficulty = GameAndDifficulty()
     var balloonViews = [UIImageView]()
+    var numberViews = [UIImageView]()
     var time = Time(seconds: 0)
+    
+    var balloonTimer = Timer()
+    var balloonIsBeingUsed = [Bool](repeatElement(false, count: 10))
+    
     
     var scoreTimer = Timer()
     var scoreSeconds = 0
     var finalScore = 0
+    
+    var pop: UITapGestureRecognizer!
+    
+    let numberImageArray: [UIImage] = [
+        UIImage(named: "cartoon-number-0")!,
+        UIImage(named: "cartoon-number-1")!,
+        UIImage(named: "cartoon-number-2")!,
+        UIImage(named: "cartoon-number-3")!,
+        UIImage(named: "cartoon-number-4")!,
+        UIImage(named: "cartoon-number-5")!,
+        UIImage(named: "cartoon-number-6")!,
+        UIImage(named: "cartoon-number-7")!,
+        UIImage(named: "cartoon-number-8")!,
+        UIImage(named: "cartoon-number-9")!,]
+    
+    var balloonImage: [UIImage] = [
+        UIImage(named:"color1")!,
+        UIImage(named:"color2")!,
+        UIImage(named:"color3")!,
+        UIImage(named:"color4")!,
+        UIImage(named:"color5")!,
+        UIImage(named:"color6")!,
+        UIImage(named:"color7")!,
+        UIImage(named:"color8")!,
+        UIImage(named:"color9")!,
+        UIImage(named:"color10")!]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,16 +102,26 @@ class BalloonViewController: UIViewController {
     
     func setUp10Locations() {
         for i in 0..<10 {
-            let balloonSpace = UIView(frame: CGRect(x:20 + (100*i), y: 700, width: 80, height: 80))
-            balloonSpace.backgroundColor = UIColor.black
+            let balloonSpace = UIImageView(frame: CGRect(x:20 + (100*i), y: 800, width: 80, height: 80))
+            let numberSpace = UIImageView(frame: CGRect(x:25, y:20, width: 30, height: 40))
+            
+//            balloonSpace.backgroundColor = UIColor.black
+//            numberSpace.backgroundColor = UIColor.red
+            
+            createPopAnim(balloon: balloonSpace)
             self.view.addSubview(balloonSpace)
+            balloonSpace.addSubview(numberSpace)
+            numberViews.append(numberSpace)
+            balloonViews.append(balloonSpace)
+            
         }
     }
     
     func startTime() {
         time = setUpTimer()
-        time.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(SortingViewController.updateTimeImages)), userInfo: nil, repeats: true)
-        scoreTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(SortingViewController.updateScoreTime)), userInfo: nil, repeats: true)
+        time.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(BalloonViewController.updateTimeImages)), userInfo: nil, repeats: true)
+        scoreTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(BalloonViewController.updateScoreTime)), userInfo: nil, repeats: true)
+        balloonTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(BalloonViewController.balloonRand)), userInfo: nil, repeats: true)
     }
     
     func setUpTimer() -> Time {
@@ -111,8 +152,69 @@ class BalloonViewController: UIViewController {
         }
     }
     
+    func createPopAnim(balloon: UIView) {
+        balloon.isUserInteractionEnabled = true
+        pop = UITapGestureRecognizer(target: self, action: (#selector(popBalloon)))
+        balloon.addGestureRecognizer(pop)
+    }
+    
+    @objc func popBalloon() {
+        print("pop")
+    }
+    
+    @objc func balloonRand() {
+        var randLocation = Int(arc4random_uniform(10))
+        var randBalloon = Int(arc4random_uniform(10))
+        
+        while balloonIsBeingUsed[randLocation]{
+            randLocation = Int(arc4random_uniform(10))
+        }
+        let balloonImageView = balloonViews[randLocation]
+        let numberImageView = numberViews[randLocation]
+ //       let originalLoc = CGRect(x: balloonViews[randLocation].frame.origin.x + self.view.frame.origin.x, y: 700, width: 80, height: 80)
+//        let newLoc = CGRect(x: balloonViews[randLocation].frame.origin.x + self.view.frame.origin.x, y: -100, width: 80, height: 80)
+        var i = 0
+        
+        balloonImageView.image = balloonImage[randBalloon]
+        numberImageView.image = numberImageArray[randBalloon]
+        
+    //    createPopAnim(balloon: balloonImageView)
+        
+  //      while balloonImageView.frame.origin.y > -100 && i < 50{
+        //    print (balloonImageView.frame.origin.y)
+  //          print (i)
+
+            UIView.animate(withDuration: 9,
+                           delay: 0,
+                           options: [.allowUserInteraction, .allowAnimatedContent],
+                           animations: {
+                            self.balloonIsBeingUsed[randLocation] = true
+//                                balloonImageView.frame.origin.y = balloonImageView.frame.origin.y - CGFloat(i)
+                                balloonImageView.frame.origin.y = -100
+                            },
+                           completion: {(finished: Bool) in
+                            balloonImageView.frame.origin.y = 800
+                            self.balloonIsBeingUsed[randLocation] = false
+            })
+
+ //           i += 1
+  //      }
+    }
+    
+
+    
+
+    
+    
+    
     @objc func updateScoreTime() {
         scoreSeconds = scoreSeconds + 1
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        time.timer.invalidate()
+        scoreTimer.invalidate()
+        balloonTimer.invalidate()
     }
     
 
